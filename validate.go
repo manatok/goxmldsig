a09a0f01503d85811ed6ef9dcfeb8c3ca9eb278f
David Berliner <dsberliner@gmail.com>
2020-06-05 15:14:13 +0200

    Removed the validation on 2 transforms.

--- validate.go
+++ validate.go
@@ -110,10 +110,6 @@ func (ctx *ValidationContext) transform(
 	ref *types.Reference) (*etree.Element, Canonicalizer, error) {
 	transforms := ref.Transforms.Transforms
 
-	if len(transforms) != 2 {
-		return nil, nil, errors.New("Expected Enveloped and C14N transforms")
-	}
-
 	// map the path to the passed signature relative to the passed root, in
 	// order to enable removal of the signature by an enveloped signature
 	// transform
